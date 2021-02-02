772e663040b61527925eae659a8302562ffd1090
viniciusleonardovky <vile.pb@gmail.com>
2021-02-02 02:50:44 -0300

    styles: register transaction page

--- src/pages/Transaction/index.tsx
+++ src/pages/Transaction/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import Header from '../../components/Header';
+
 import { Container } from './styles';
 
 const Transaction: React.FC = () => {
   return (
-    <Container>
-      <h1>Transactions</h1>
-    </Container>
+    <>
+      <Header size="small" />
+
+      <Container>
+        <form onSubmit={() => console.log('')}>
+          <h2>Cadastre uma transação</h2>
+          <input type="text" placeholder="Título" />
+
+          <input type="text" placeholder="Valor" />
+
+          <select name="type" id="type">
+            <option value="0">Tipo de transação...</option>
+            <option value="income">Entrada</option>
+            <option value="outcome">Saída</option>
+          </select>
+
+          <select name="category" id="category">
+            <option value="0">Categoria...</option>
+            <option value="income">Comidas</option>
+            <option value="outcome">Transporte</option>
+            <option value="outcome">Outros</option>
+          </select>
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Container>
+    </>
   );
 };
 
--- src/pages/Transaction/styles.ts
+++ src/pages/Transaction/styles.ts
@@ -1,5 +1,73 @@
 import styled from 'styled-components';
+import { darken, lighten } from 'polished';
 
 export const Container = styled.div`
+  margin-top: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
+  form {
+    width: 100%;
+    max-width: 350px;
+    display: flex;
+    flex-direction: column;
+
+    h2 {
+      align-self: center;
+      color: #222;
+    }
+
+    input {
+      margin-top: 20px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      height: 48px;
+      padding: 0 20px;
+      font-size: 16px;
+      color: #222;
+
+      & + input {
+        margin-top: 5px;
+      }
+    }
+
+    select {
+      background-color: #fff;
+      margin-top: 5px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      height: 48px;
+      padding: 0 20px;
+      font-size: 16px;
+      color: #222;
+    }
+
+    button {
+      margin-top: 10px;
+      border: 0;
+      border-radius: 4px;
+      height: 48px;
+      font-size: 16px;
+      background: #3bafda;
+      font-weight: bold;
+      color: #fff;
+
+      &:hover {
+        background-color: ${darken(0.08, '#3bafda')};
+      }
+    }
+  }
+
+  a {
+    margin-top: 5px;
+    text-decoration: none;
+    color: #222;
+
+    &:hover {
+      color: ${lighten(0.08, '#222')};
+    }
+  }
 `;
