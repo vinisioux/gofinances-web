1efa16979b335d009adf7327de51adf1b7396646
viniciusleonardovky <vile.pb@gmail.com>
2021-02-02 02:26:40 -0300

    feat: create transaction page

---
+++ src/pages/Transaction/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Transaction: React.FC = () => {
+  return (
+    <Container>
+      <h1>Transactions</h1>
+    </Container>
+  );
+};
+
+export default Transaction;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
+import Transaction from '../pages/Transaction';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -14,6 +15,7 @@ const Routes: React.FC = () => (
     <Route path="/signup" component={SignUp} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
     <Route path="/import" component={Import} isPrivate />
+    <Route path="/transaction" component={Transaction} isPrivate />
   </Switch>
 );
 
